0cb1a2e52fac16db5a4c581b0a24b9e537c4e8d9
commit <ncsakura892@gmail.com>
2025-12-17 13:35:17 +0600

    Initial commit

--- src/Trugolnic.java
+++ src/Trugolnic.java
@@ -3,11 +3,13 @@ public class Trugolnic {
     int a;
     int b;
     int c;
-    public void ploshat (){
 
-        System.out.println(a*b*c/2);
+
+    public void ploshat() {
+            double p = (a + b + c) / 2.0;
+            System.out.println(p);
+
     }
-
-
-
 }
+
+
